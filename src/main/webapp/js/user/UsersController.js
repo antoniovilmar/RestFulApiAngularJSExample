9879289e542d218c65ac35a134820ee255cdd855
Antonio Castro <antoniovilmar.castro@gmail.com>
2016-06-04 02:16:21 -0300

    Feature remover usuário implementada no front

--- src/main/webapp/js/user/UsersController.js
+++ src/main/webapp/js/user/UsersController.js
@@ -2,6 +2,7 @@ app.controller('UsersController', function($scope, $http) {
 
     $scope.users = [];
     $scope.filtro = '';
+    $scope.mensagem = '';
 
     var promise = $http.get('http://localhost:8080/user/');
     promise.then(function(retorno) {
@@ -11,4 +12,19 @@ app.controller('UsersController', function($scope, $http) {
             console.log(erro)
         });
 
+    $scope.remover = function(user) {
+
+        $http.delete('http://localhost:8080/user/' + user.id)
+            .success(function() {
+                var indiceDoUser = $scope.users.indexOf(user);
+                $scope.users.splice(indiceDoUser, 1);
+                $scope.mensagem = 'Usuário ' + user.name + ' removido com sucesso!';
+
+            })
+            .error(function(erro) {
+                console.log(erro);
+                $scope.mensagem = 'Não foi possível apagar o Usuário ' + user.name
+            });
+
+    }
 });
